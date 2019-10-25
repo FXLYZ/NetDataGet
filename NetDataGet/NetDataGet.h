fe5457bf64f1274174a8a78f6e9358df6546c1a3
FXLYZ <765189431@qq.com>
2019-10-25 15:32:06 +0800

    first

---
+++ NetDataGet/NetDataGet.h
@@ -0,0 +1,32 @@
+﻿
+// NetDataGet.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CNetDataGetApp:
+// 有关此类的实现，请参阅 NetDataGet.cpp
+//
+
+class CNetDataGetApp : public CWinApp
+{
+public:
+	CNetDataGetApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CNetDataGetApp theApp;
---
+++ NetDataGet/NetDataGetDlg.h
@@ -0,0 +1,42 @@
+﻿
+// NetDataGetDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CNetDataGetDlg 对话框
+class CNetDataGetDlg : public CDialogEx
+{
+// 构造
+public:
+	CNetDataGetDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_NETDATAGET_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CEdit DataContent;
+	CListBox NetWorkCardBox;
+	CEdit NetWorkCardInfo;
+	CButton StartCatch;
+	CButton StopCatch;
+	afx_msg void OnBnClickedButtonStart();
+	afx_msg void OnBnClickedButtonStop();
+};
